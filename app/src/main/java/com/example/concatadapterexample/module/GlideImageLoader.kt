3e16cb99537acf2d0ededc903f7ce9ed48f5070b
Zeeshan Rasool <m.zeeshan.rasool@gmail.com>
2021-09-12 17:23:50 +0400

    Kotlin Upgrade is in progress

--- app/src/main/java/com/example/concatadapterexample/module/GlideImageLoader.kt
+++ app/src/main/java/com/example/concatadapterexample/module/GlideImageLoader.kt
@@ -1,84 +1,71 @@
-package com.example.concatadapterexample.module;
+package com.example.concatadapterexample.module
 
-import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+import android.graphics.drawable.Drawable
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.example.concatadapterexample.module.ProgressAppGlideModule.UIonProgressListener
 
-import android.graphics.drawable.Drawable;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-
-import androidx.annotation.Nullable;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.Target;
-
-public class GlideImageLoader {
-
-    private ImageView mImageView;
-    private ProgressBar mProgressBar;
-
-    public GlideImageLoader(ImageView imageView, ProgressBar progressBar) {
-        mImageView = imageView;
-        mProgressBar = progressBar;
-    }
-
-    public void load(final String url, RequestOptions options) {
-        if (url == null || options == null) return;
-
-        onConnecting();
+class GlideImageLoader(private val mImageView: ImageView, private val mProgressBar: ProgressBar) {
+    fun load(url: String?, options: RequestOptions?) {
+        if (url == null || options == null) return
+        onConnecting()
 
         //set Listener & start
-        ProgressAppGlideModule.expect(url, new ProgressAppGlideModule.UIonProgressListener() {
-            @Override
-            public void onProgress(long bytesRead, long expectedLength) {
-                if (mProgressBar != null) {
-                    mProgressBar.setProgress((int) (100 * bytesRead / expectedLength));
-                }
+        ProgressAppGlideModule.expect(url, object : UIonProgressListener {
+            override fun onProgress(bytesRead: Long, expectedLength: Long) {
+                mProgressBar.progress = (100 * bytesRead / expectedLength).toInt()
             }
 
-            @Override
-            public float getGranualityPercentage() {
-                return 1.0f;
+            override fun getGranualityPercentage(): Float {
+                return 1.0f
             }
-        });
+        })
         //Get Image
-        Glide.with(mImageView.getContext())
-                .load(url)
-                .transition(withCrossFade())
-                .apply(options.skipMemoryCache(true))
-                .listener(new RequestListener<Drawable>() {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        ProgressAppGlideModule.forget(url);
-                        onFinished();
-                        return false;
-                    }
+        Glide.with(mImageView.context)
+            .load(url)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .apply(options.skipMemoryCache(true))
+            .listener(object : RequestListener<Drawable?> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any,
+                    target: Target<Drawable?>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    ProgressAppGlideModule.forget(url)
+                    onFinished()
+                    return false
+                }
 
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        ProgressAppGlideModule.forget(url);
-                        onFinished();
-                        return false;
-                    }
-                })
-                .into(mImageView);
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any,
+                    target: Target<Drawable?>,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    ProgressAppGlideModule.forget(url)
+                    onFinished()
+                    return false
+                }
+            })
+            .into(mImageView)
     }
 
-
-    private void onConnecting() {
-        if (mProgressBar != null) {
-            mProgressBar.setVisibility(View.VISIBLE);
-        }
+    private fun onConnecting() {
+        mProgressBar.visibility = View.VISIBLE
     }
 
-    private void onFinished() {
-        if (mProgressBar != null && mImageView != null) {
-            mProgressBar.setVisibility(View.GONE);
-            mImageView.setVisibility(View.VISIBLE);
-        }
+    private fun onFinished() {
+        mProgressBar.visibility = View.GONE
+        mImageView.visibility = View.VISIBLE
     }
 }
